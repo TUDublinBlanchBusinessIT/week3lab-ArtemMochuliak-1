1a5e0dceff9c393ffbb4295638fb34c8762744ff
B00165618 Artem Mochuliak <B00165618@mytudublin.ie>
2025-09-29 15:49:29 +0100

    step 1

--- App.js
+++ App.js
@@ -1,16 +1,43 @@
-import {View, Text, TextInput} from 'react-native';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import {StyleSheet,  Text, View , TextInput} from 'react-native';
+import Logo from './components/Logo'
 
 
-export default function App(){
-  const [fullname, setFullname] = useState("Eoin");
-  return(
-    <View> 
-      <Text>Hello, World {fullname}</Text>
-      <TextInput placeholder="enter your name" onChangeText={(value)=>setFullname(value)}></TextInput>
+export default function App() {
+  const [fullname, setFullname] = useState("Artem Mochuliak");
+  return (
+   <View style={styles.container}>
+   <Logo/>
+      <Text style={styles.paragraph}>Hello, World {fullname}</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Enter your name"
+        onChangeText={text => setFullname(text)}
+        value={fullname}
+      />
     </View>
-  )
-};
+  );
+}
 
-
+const styles = StyleSheet.create({
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+      
+  },
+    
+  container: {
+    padding: 24,
+  },
+    
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    paddingHorizontal: 8,
+  },  
+});
 
