b470aae256da0ffea2b6d1b3dcc7a968559f9ddc
B00165618 Artem Mochuliak <B00165618@mytudublin.ie>
2025-09-29 16:12:06 +0100

    step 2

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import React, { useState } from 'react';
-import {StyleSheet,  Text, View , TextInput} from 'react-native';
-import Logo from './components/Logo'
-
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import Logo from './components/Logo';
 
 export default function App() {
   const [fullname, setFullname] = useState("Artem Mochuliak");
+  const [fname, setFname] = useState("Joe");
+  const [lname, setLname] = useState("Bloggs");
+  const [dob, setDob] = useState("22 August 1990");
+
   return (
-   <View style={styles.container}>
-   <Logo/>
+    <View style={styles.container}>
+      <Logo />
+
       <Text style={styles.paragraph}>Hello, World {fullname}</Text>
 
       <TextInput
@@ -16,28 +20,55 @@ export default function App() {
         onChangeText={text => setFullname(text)}
         value={fullname}
       />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Enter your firstname"
+        onChangeText={setFname}
+        value={fname}
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Enter your lastname"
+        onChangeText={setLname}
+        value={lname}
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Enter your date of birth"
+        onChangeText={setDob}
+        value={dob}
+      />
+
+      <Text style={styles.outputText}>
+        Hello {fname} {lname}. You were born on {dob}.
+      </Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    padding: 24,
+  },
   paragraph: {
     margin: 24,
     fontSize: 18,
     fontWeight: 'bold',
     textAlign: 'center',
-      
   },
-    
-  container: {
-    padding: 24,
-  },
-    
   input: {
     height: 40,
     borderColor: 'gray',
     borderWidth: 1,
     paddingHorizontal: 8,
-  },  
+    marginBottom: 12,
+  },
+  outputText: {
+    marginTop: 20,
+    fontSize: 16,
+    textAlign: 'center',
+  },
 });
-
